788ed0cb782198ed1a774975a20f814d4dd9c792
JangGyuSik <jgs288@gmail.com>
2024-06-04 20:15:01 +0900

    오타 수정

---
+++ app/src/main/java/com/example/donotlate/map/SeachPlaceFragment.kt
@@ -0,0 +1,170 @@
+package com.example.donotlate.map
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import com.example.donotlate.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+
+class SeachPlaceFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mGoogleMap: GoogleMap
+    private lateinit var mapView : MapView
+    //위치 서비스가 gps를 사용해서 위치를 확인
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    //위치 값 요청에 대한 갱신 정보를 받는 변수
+    lateinit var locationCallback: LocationCallback
+
+    lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        locationPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){ results ->
+            if(results.all{it.value}){
+                val rootView = inflater.inflate(R.layout.fragment_seach_place,container,false)
+                mapView = rootView.findViewById(R.id.map) as MapView
+                mapView.onCreate(savedInstanceState)
+                mapView.getMapAsync(this)
+            }else{ //문제가 발생했을 때
+                Toast.makeText(requireActivity(),"권한 승인이 필요합니다.",Toast.LENGTH_LONG).show()
+            }
+        }
+
+        //권한 요청
+        locationPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seach_place, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SeachPlacetFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SeachPlaceFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        val seoul = LatLng(37.566610, 126.978403)
+        mGoogleMap = p0
+        mGoogleMap.mapType = GoogleMap.MAP_TYPE_NORMAL // default 노말 생략 가능
+        mGoogleMap.apply {
+            val markerOptions = MarkerOptions()
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            markerOptions.position(seoul)
+            markerOptions.title("서울시청")
+            markerOptions.snippet("Tel:01-120")
+            addMarker(markerOptions)
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        updateLocation()
+    }
+    fun updateLocation(){
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 500
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback(){
+            //1초에 한번씩 변경된 위치 정보가 onLocationResult 으로 전달된다.
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult?.let{
+                    for (location in it.locations){
+                        Log.d("위치정보",  "위도: ${location.latitude} 경도: ${location.longitude}")
+                        setLastLocation(location) //계속 실시간으로 위치를 받아오고 있기 때문에 맵을 확대해도 다시 줄어든다.
+                    }
+                }
+            }
+        }
+        //권한 처리
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback,
+            Looper.myLooper()!!
+        )
+    }
+
+    fun setLastLocation(lastLocation: Location){
+        val LATLNG = LatLng(lastLocation.latitude,lastLocation.longitude)
+
+        val makerOptions = MarkerOptions().position(LATLNG).title("나 여기 있어용~")
+        val cameraPosition = CameraPosition.Builder().target(LATLNG).zoom(15.0f).build()
+
+        mGoogleMap.addMarker(makerOptions)
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+}
