f340c66c261af55db808d2c7d52f03f79d96c853
LimChaem <saver203@naver.com>
2024-06-22 01:17:45 +0900

    fix: code clean up

--- app/src/main/java/com/example/donotlate/core/data/repository/PromiseRoomRepositoryImpl.kt
+++ app/src/main/java/com/example/donotlate/core/data/repository/PromiseRoomRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.donotlate.core.data.repository
 
 import android.util.Log
 import com.example.donotlate.core.domain.repository.PromiseRoomRepository
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
@@ -17,30 +18,22 @@ class PromiseRoomRepositoryImpl(private val db: FirebaseFirestore) : PromiseRoom
         flow {
             try {
                 val roomRef = db.collection("PromiseRooms").document(roomId)
-                Log.d("나가기", "실행 1 ${roomRef}")
 
                 db.runTransaction { transaction ->
-                    Log.d("나가기", "실행 2")
 
                     val snapshot = transaction.get(roomRef)
                     val participants = snapshot.get("participants") as? List<String> ?: listOf()
-                    Log.d("나가기", "실행 3 ${participants}")
                     val mutableParticipants = participants.toMutableList()
 
                     mutableParticipants.remove(participantId)
-                    Log.d("나가기", "제거 ${mutableParticipants}")
-                    Log.d("나가기", "제거 ${participantId}")
 
                     if (mutableParticipants.isEmpty()) {
                         transaction.delete(roomRef)
-                        Log.d("나가기", "실행 4 나감")
                     } else {
                         transaction.update(roomRef, "participants", mutableParticipants)
                     }
                 }.await()
                 emit(true)
-                Log.d("나가기", "실행 5")
-
             } catch (e: Exception) {
                 emit(false)
             }
