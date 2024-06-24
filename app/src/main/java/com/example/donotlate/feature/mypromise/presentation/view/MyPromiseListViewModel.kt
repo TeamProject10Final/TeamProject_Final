16df489db17b0c8af71ca2c91ea5af7c078581b7
LimChaem <saver203@naver.com>
2024-06-24 16:52:11 +0900

    fix : 1. promiseTime 정보 양식 변경으로 인한 파싱오류 - 기존 hh시 mm분 -> 오전(또는 오후) hh시 mm분으로 변경되면서 기존 파싱 코드가 작동을 안하는 오류 발생, - 파싱 코드 변경으로 오류 해결

--- app/src/main/java/com/example/donotlate/feature/mypromise/presentation/view/MyPromiseListViewModel.kt
+++ app/src/main/java/com/example/donotlate/feature/mypromise/presentation/view/MyPromiseListViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.donotlate.core.domain.usecase.LoadToMyPromiseListUseCase
 import com.example.donotlate.core.presentation.CurrentUser
+import com.example.donotlate.core.util.parseTime
 import com.example.donotlate.feature.mypromise.presentation.mapper.toPromiseModelList
 import com.example.donotlate.feature.mypromise.presentation.model.PromiseModel
 import kotlinx.coroutines.Job
@@ -14,7 +15,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class MyPromiseListViewModel(
@@ -45,21 +45,17 @@ class MyPromiseListViewModel(
                     loadToMyPromiseListUseCase(uid).collect {
                         val promiseRooms = it.toPromiseModelList()
                         _promiseRoomList.value = promiseRooms
-                        Log.d("PromiseList", "${_promiseRoomList.value}")
 
-                        // 약속 날짜와 시간을 String -> LocalDate 타입으로 변경하기 위한 포맷팅
+//                         약속 날짜와 시간을 String -> LocalDate 타입으로 변경하기 위한 포맷팅
                         val now = LocalDateTime.now()
                         val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-                        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
                         // 약속 시간과 날짜를 현재 시간과 날짜와 비교한 값
                         // '현재 시간과 날짜'보다 이후의 약속들을 필터링 하기 위한 코드.
                         val upcomingPromise = promiseRooms.filter { rooms ->
                             val promiseDate = LocalDate.parse(rooms.promiseDate, dateFormatter)
-                            val promiseTimeString =
-                                rooms.promiseTime.replace("시", ":").replace("분", "")
-                                    .replace(" ", "")
-                            val promiseTime = LocalTime.parse(promiseTimeString, timeFormatter)
+                            val promiseTime = parseTime(rooms.promiseTime)
+
                             val promiseDateTime = LocalDateTime.of(promiseDate, promiseTime)
                             promiseDateTime.isAfter(now)
                         }
@@ -68,10 +64,8 @@ class MyPromiseListViewModel(
                         val closestPromise = upcomingPromise.minByOrNull {
                             // 파싱을 위한 코드.
                             val promiseDate = LocalDate.parse(it.promiseDate, dateFormatter)
-                            val promiseTimeString =
-                                it.promiseTime.replace("시", ":").replace("분", "").replace(" ", "")
-
-                            val promiseTime = LocalTime.parse(promiseTimeString, timeFormatter)
+                            val promiseTime = parseTime(it.promiseTime)
+                            Log.d("시간확인", "$promiseTime")
 
                             LocalDateTime.of(promiseDate, promiseTime)
                         }
@@ -96,3 +90,4 @@ class MyPromiseListViewModelFactory(
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
+
