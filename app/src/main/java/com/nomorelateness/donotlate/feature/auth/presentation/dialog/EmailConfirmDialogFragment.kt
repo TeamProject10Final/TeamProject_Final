4644c6f817e308c17e9f56d4fbd742343f8b1d48
JangGyuSik <jgs288@gmail.com>
2024-07-01 11:47:11 +0900

    이메일 인증 다이얼로그

---
+++ app/src/main/java/com/nomorelateness/donotlate/feature/auth/presentation/dialog/EmailConfirmDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.nomorelateness.donotlate.feature.auth.presentation.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.nomorelateness.donotlate.R
+import com.nomorelateness.donotlate.databinding.FragmentEmailConfirmDialogBinding
+
+
+class EmailConfirmDialogFragment : DialogFragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentEmailConfirmDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentEmailConfirmDialogBinding.inflate(inflater, container, false)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        return binding.root
+    }
+
+
+}
